b26979a05bbc84eaf84d42e1c0803160a55ba6d4
Brayan <alfredochabes1@hotmail.com>
2022-11-16 11:01:30 -0500

    merge complete

--- client/src/components/Modal/ModalLogin/Modal.js
+++ client/src/components/Modal/ModalLogin/Modal.js
@@ -2,28 +2,6 @@ import React from "react";
 import Home from "../../../pages/Home";
 import Login from "../../../pages/Login";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-import "./Modal.css";
-
-export default function ModalLogin() {
-  document.body.classList.add("active-modal");
-
-  return (
-    <>
-      <Home />
-      {true && (
-        <div className="modal">
-          <div className="overlay"></div>
-          <div className="modal-content">
-            <Login />
-            <button className="close-modal-login">
-              <Link to="/">Close</Link>
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-=======
 import { useStoreContext } from "../../../utils/GlobalState";
 import { TOGGLE_MODAL } from "../../../utils/actions";
 import "./ModalLogin.css";
@@ -54,6 +32,5 @@ export default function ModalLogin() {
         </div>
       )}
     </div>
->>>>>>> 234133f039bda8ebbb094bc3fd9dc4bb8e6365be
   );
 }
