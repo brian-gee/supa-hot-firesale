356c2a9a6db7431654a15e95fce7a326ce87f3f6
dopecello <williamlocke.cello@gmail.com>
2022-11-15 23:18:15 -0500

    modal for login and signup complete

--- client/src/components/Modal/ModalLogin/Modal.js
+++ client/src/components/Modal/ModalLogin/Modal.js
@@ -1,30 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import Home from "../../../pages/Home";
 import Login from "../../../pages/Login";
+import { Link } from "react-router-dom";
 import { useStoreContext } from "../../../utils/GlobalState";
 import { TOGGLE_MODAL } from "../../../utils/actions";
-import "./Modal.css";
+import "./ModalLogin.css";
 
-export default function ModalLogin({ modalIsOn }) {
-    // document.body.classList.add('active-modal')
-    const [state, dispatch] = useStoreContext()
+export default function ModalLogin() {
+  // document.body.classList.add('active-modal')
+  const [state, dispatch] = useStoreContext();
 
-    function toggleModal() {
-      dispatch({ type: TOGGLE_MODAL });
-      document.body.classList.add('active-modal')
-    }
-  
+  function toggleModal() {
+    dispatch({ type: TOGGLE_MODAL });
+  }
+
   return (
+    <div>
       <Home />
-      {state.modalOpen ? (
-        <div className="modal">
-          <div  className="overlay"></div>
-          <div className="modal-content">
-            <Login />
-            <button className="close-modal" >
-              CLOSE
-            </button>
+      {state.modalOpen ? null : (
+        <div>
+          <div className="modal">
+            <div className="overlay">
+              <div className="modal-content">
+                <Login />
+                <button className="close-modal" onClick={toggleModal}>
+                  <Link to="/" onClick={toggleModal}>CLOSE</Link>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      ) : (null) }
+      )}
+    </div>
   );
+}
--- client/src/components/Modal/ModalSignup/Modal.js
+++ client/src/components/Modal/ModalSignup/Modal.js
@@ -1,28 +1,37 @@
-import React, { useState} from "react";
+import React from "react";
 import Home from "../../../pages/Home";
-
-import Login from "../../../pages/Login";
 import Signup from "../../../pages/Signup";
-import "./Modal.css";
+import { Link } from "react-router-dom";
+import { useStoreContext } from "../../../utils/GlobalState";
+import { TOGGLE_MODAL } from "../../../utils/actions";
+import "./ModalSignup.css";
 
-export default function ModalSignUp() {
-  document.body.classList.add('active-modal')
+export default function ModalSignup() {
+  const [state, dispatch] = useStoreContext();
+
+  function toggleModal() {
+    dispatch({ type: TOGGLE_MODAL });
+  }
 
   return (
-    <>
+    <div>
       <Home />
-      {true && (
-        <div className="modal">
-          <div  className="overlay"></div>
-          <div className="modal-content">
-            <Signup />
-            <button className="close-modal" >
-              CLOSE
-            </button>
+      {state.modalOpen ? null : (
+        <div>
+          <div className="modal">
+            <div className="overlay">
+              <div className="modal-content">
+                <Signup />
+                <button className="close-signupmodal" onClick={toggleModal}>
+                  <Link to="/" onClick={toggleModal}>
+                    CLOSE
+                  </Link>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
-     
-    </>
+    </div>
   );
 }
